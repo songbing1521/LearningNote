38903df767f1ddbb125c74a486f75e0b665e228a
songbing <songbing1521@163.com>
2023-03-12 02:10:00 +0800

    Python练习添加

---
+++ .history/Demo/Python_Demo/Python_Demo5_20230312015647.py
@@ -0,0 +1,6 @@
+# 寻找水仙花数
+x = input("请输入数字")
+if int(x[0])**3 + int(x[1])**3+int(x[2])**3 == int(x):
+    print("%s 是水仙花数" % x)
+else:
+    print("%s 不是水仙花数" % x)
---
+++ .history/Demo/Python_Demo/Python_Demo5_20230312020358.py
@@ -0,0 +1,10 @@
+# 寻找水仙花数
+"""
+    x = input("请输入数字")
+if int(x[0])**3 + int(x[1])**3+int(x[2])**3 == int(x):
+    print("%s 是水仙花数" % x)
+else:
+    print("%s 不是水仙花数" % x)
+
+"""
+for()
