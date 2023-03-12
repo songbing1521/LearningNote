e8ea020c1b66808fa0bbdcaa89d46bbad2a0c413
songbing <songbing1521@163.com>
2023-03-13 00:22:22 +0800

    Python_function add

--- .history/Demo/Python_Demo/Python_Demo5_20230312120822.py
+++ .history/Demo/Python_Demo/Python_Demo5_20230312120822.py
@@ -18,4 +18,3 @@ for i in range(0, 101):
         for k in range(0, 101,):
             if (5*i + 3*j + k/3) == 100 and i+j+k == 100:
                 print("%d %d %d" % (i, j, k))
-                
