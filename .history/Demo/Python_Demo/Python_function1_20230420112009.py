189ca9dcff8aa79cd8dd3cf088754bc317714ad7
songbing <songbing1521@163.com>
2023-04-24 13:57:43 +0800

    Python_function

--- .history/Demo/Python_Demo/Python_function1_20230420112009.py
+++ .history/Demo/Python_Demo/Python_function1_20230420112009.py
@@ -3,8 +3,3 @@ def swap(a, b):
     b = a - b
     a = a - b
     return a, b
-
-
-a, b = map(int, input().split())
-a, b = swap(a, b)
-print(a, b)
--- .history/Demo/Python_Demo/Python_function1_20230420112110.py
+++ .history/Demo/Python_Demo/Python_function1_20230420112110.py
@@ -6,5 +6,5 @@ def swap(a, b):
 
 
 a, b = map(int, input().split())
-a, b = swap(a, b)
+swap(a, b)
 print(a, b)
