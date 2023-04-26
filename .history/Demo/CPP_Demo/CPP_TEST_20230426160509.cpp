3b85ba00c9e22fbdc5482766774e547543ff14b2
songbing <songbing1521@163.com>
2023-04-26 16:25:43 +0800

    cpp_and

---
+++ .history/Demo/CPP_Demo/CPP_TEST_20230426160509.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double m = 0, n = 0;
+    cin >> m >> n;
+    int k = 0;
+    while(m - n*k >= n){
+        k++;
+    }
+    cout << m-n*k << " " <<endl;
+}
---
+++ Demo/CPP_Demo/CPP_TEST.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main(){
+   double m , n;
+   cin >> m >> n;
+   printf("%.2lf\n", (m*n)/(m+n));
+}
